b82127a0df1102c1196a2aa5da5f8c9a5e708745
Kirill Tepljakov <super.css2017@yandex.ru>
2023-06-07 13:15:01 +0300

    заверстал header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./scss/app.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <header className="header">
+          <div className="top-navigation">
+            <div className="container">
+              <div className="content flex">
+                <nav className="navigation">
+                  <ul className="flex">
+                    <li>Главная</li>
+                    <li>О нас</li>
+                    <li>Контакты</li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
     </div>
   );
 }
