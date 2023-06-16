8ba9a19f8b8e3c7f98e1c48852eb3f1c45ce8e2c
Kirill Tepljakov <super.css2017@yandex.ru>
2023-06-16 10:43:35 +0300

    заверстал карточку товара

--- src/Components/Bestsellers.jsx
+++ src/Components/Bestsellers.jsx
@@ -1,34 +1,39 @@
 import React from "react";
+import stul from "../img/product-1.jpg";
 import parameter from "../img/parameter.png";
 
 function Bestsellers() {
   return (
     <div className="container">
       <div className="bestsellers">
-        <div className="card">
-          <img src={stul} alt="Продукт" />
-          <div className="card__description">
-            <p className="title">Валенсия</p>
-            <p className="descr">Барные стулья</p>
-            <p className="price">2 300₽</p>
-          </div>
-          <div className="card__size">
-            <p className="size-title">Размеры</p>
-            <div className="size-block flex">
-              <div className="size">
-                <p className="size__title">ширина</p>
-                <p className="size__width">43см</p>
+        <p className="bestsellers__title">Хиты продаж</p>
+        <div className="card-block flex">
+          <div className="card">
+            <img className="card__img" src={stul} alt="Продукт" />
+            <div className="card__description">
+              <p className="title">Валенсия Beige</p>
+              <p className="descr">Барные стулья</p>
+              <p className="price">2 300₽</p>
+            </div>
+            <div className="card__size">
+              <p className="size-title">Размеры</p>
+              <div className="size-block flex">
+                <div className="size">
+                  <p className="size__title">ширина</p>
+                  <p className="size__width">43см</p>
+                </div>
+                <img src={parameter} alt="Размер" />
+                <div className="size">
+                  <p className="size__title">глубина</p>
+                  <p className="size__width">43см</p>
+                </div>
+                <img src={parameter} alt="Размер" />
+                <div className="size">
+                  <p className="size__title">высота</p>
+                  <p className="size__width">43см</p>
+                </div>
               </div>
-              <img src={parameter} alt="Размер" />
-              <div className="size">
-                <p className="size__title">глубина</p>
-                <p className="size__width">43см</p>
-              </div>
-              <img src={parameter} alt="Размер" />
-              <div className="size">
-                <p className="size__title">высота</p>
-                <p className="size__width">43см</p>
-              </div>
+              <button>Добавить в корзину</button>
             </div>
           </div>
         </div>
