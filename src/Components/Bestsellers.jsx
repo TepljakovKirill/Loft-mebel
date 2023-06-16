32098d82ec78400fa270f3cf5fb4c4dc81830b59
Kirill Tepljakov <super.css2017@yandex.ru>
2023-06-16 12:51:45 +0300

    Доверстал карточку и отрендерил список карточек

--- src/Components/Bestsellers.jsx
+++ src/Components/Bestsellers.jsx
@@ -1,41 +1,26 @@
 import React from "react";
-import stul from "../img/product-1.jpg";
-import parameter from "../img/parameter.png";
+import Card from "./Card";
+import cards from "../../src/cards.json";
 
 function Bestsellers() {
+  console.log(cards);
   return (
     <div className="container">
       <div className="bestsellers">
         <p className="bestsellers__title">Хиты продаж</p>
         <div className="card-block flex">
-          <div className="card">
-            <img className="card__img" src={stul} alt="Продукт" />
-            <div className="card__description">
-              <p className="title">Валенсия Beige</p>
-              <p className="descr">Барные стулья</p>
-              <p className="price">2 300₽</p>
-            </div>
-            <div className="card__size">
-              <p className="size-title">Размеры</p>
-              <div className="size-block flex">
-                <div className="size">
-                  <p className="size__title">ширина</p>
-                  <p className="size__width">43см</p>
-                </div>
-                <img src={parameter} alt="Размер" />
-                <div className="size">
-                  <p className="size__title">глубина</p>
-                  <p className="size__width">43см</p>
-                </div>
-                <img src={parameter} alt="Размер" />
-                <div className="size">
-                  <p className="size__title">высота</p>
-                  <p className="size__width">43см</p>
-                </div>
-              </div>
-              <button>Добавить в корзину</button>
-            </div>
-          </div>
+          {cards.map((obj) => (
+            <Card
+              key={obj.id}
+              imgUrl={obj.imgUrl}
+              title={obj.title}
+              descr={obj.descr}
+              sizeWidth={obj.sizeWidth}
+              sizeDepth={obj.sizeDepth}
+              sizeHeight={obj.sizeHeight}
+              price={obj.price}
+            />
+          ))}
         </div>
       </div>
     </div>
---
+++ src/Components/Card.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+import parameter from "../img/parameter.png";
+
+function Card({
+  id,
+  imgUrl,
+  title,
+  descr,
+  sizeWidth,
+  sizeDepth,
+  sizeHeight,
+  price,
+}) {
+  const [isFavorite, setIsFavorite] = React.useState(false);
+
+  const onClickFavorites = () => {
+    setIsFavorite(!isFavorite);
+  };
+  return (
+    <div key={id} className="card">
+      <img className="card__img" src={imgUrl} alt="Продукт" />
+      <div className="card__description">
+        <p className="title">{title}</p>
+        <p className="descr">{descr}</p>
+        <p className="price">{price}₽</p>
+      </div>
+      <div className="card__size">
+        <p className="size-title">Размеры</p>
+        <div className="size-block flex">
+          <div className="size">
+            <p className="size__title">ширина</p>
+            <p className="size__width">{sizeWidth}см</p>
+          </div>
+          <img src={parameter} alt="Размер" />
+          <div className="size">
+            <p className="size__title">глубина</p>
+            <p className="size__width">{sizeDepth}см</p>
+          </div>
+          <img src={parameter} alt="Размер" />
+          <div className="size">
+            <p className="size__title">высота</p>
+            <p className="size__width">{sizeHeight}см</p>
+          </div>
+        </div>
+        <button>Добавить в корзину</button>
+      </div>
+      <svg
+        onClick={onClickFavorites}
+        width="19"
+        height="17"
+        viewBox="0 0 19 17"
+        fill={isFavorite ? "#E92D2D" : "none"}
+      >
+        <path
+          d="M2.29442 8.54442L9.75 16L17.2056 8.54442C18.0344 7.71562 18.5 6.59152 18.5 5.41942C18.5 2.97864 16.5214 1 14.0806 1C12.9085 1 11.7844 1.46562 10.9556 2.29442L9.75 3.5L8.54442 2.29442C7.71562 1.46562 6.59152 1 5.41942 1C2.97864 1 1 2.97864 1 5.41942C1 6.59152 1.46561 7.71561 2.29442 8.54442Z"
+          stroke={isFavorite ? "none" : "black"}
+        />
+      </svg>
+    </div>
+  );
+}
+
+export default Card;
