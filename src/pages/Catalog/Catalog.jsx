e6c3e9286790ba19a430e75838a5599a1774d155
Kirill Tepljakov <super.css2017@yandex.ru>
2023-08-24 13:15:46 +0300

    компонент ListMebel совместил с компонентом MebelLink и удалил последний

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -2,19 +2,24 @@ import React from "react";
 import Bestsellers from "../../Components/Bestsellers/Bestsellers";
 import LivingRooms from "./livingRooms";
 
+export const SearchContext = React.createContext();
+
 function Catalog() {
+  const [searchValue, setSearchValue] = React.useState("");
   return (
     <div className="container">
-      <div className="container__wrap flex">
-        <div className="filter">
-          <p className="filter__title">Раздел</p>
-          <LivingRooms />
-          <p className="filter__title">Цена</p>
+      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+        <div className="container__wrap flex">
+          <div className="filter">
+            <p className="filter__title">Раздел</p>
+            <LivingRooms />
+            <p className="filter__title">Цена</p>
+          </div>
+          <div className="cards">
+            <Bestsellers />
+          </div>
         </div>
-        <div className="cards">
-          <Bestsellers />
-        </div>
-      </div>
+      </SearchContext.Provider>
     </div>
   );
 }
--- src/pages/Catalog/ListMebel.jsx
+++ src/pages/Catalog/ListMebel.jsx
@@ -1,19 +1,40 @@
 import React from "react";
 import arrow from "../../img/openfilter.png";
-import MebelLink from "./MebelLink";
 
 function ListMebel() {
   const [open, setOpen] = React.useState(false);
   const [itemMenu, setItemMenu] = React.useState(0);
+
+  const [openObj, setOpenObj] = React.useState(false);
+  const [itemObjMenu, setItemObjMenu] = React.useState(0);
+
   const listMebel = ["Мягкая мебель", "Шкафы", "Столы"];
+
+  const allMebel = ["cushionedFurniture", "closet", "table"];
+
+  const mebelObj = {
+    cushionedFurniture: ["Диваны", "Детские диваны", "Двуспальные диваны"],
+    closet: ["Комод", "Шкаф", "Буфет"],
+    table: ["Письменный стол", "Журнальный стол", "Стол школьника "],
+  };
+
+  const viewMebel = allMebel[itemMenu];
+  const mainArray = mebelObj[viewMebel];
 
   const onClickItemMenu = (i) => {
     setItemMenu(i);
     setOpen(false);
   };
 
+  const onClickLinkMenu = (i) => {
+    setItemObjMenu(i);
+    setOpenObj(false);
+  };
+
   return (
     <div className="filter__popUp">
+      {/* Меню разделов мебели */}
+
       <div className="popup__list flex">
         <div onClick={() => setOpen(!open)} className="popup__list--wrap">
           <p className="flex">{listMebel[itemMenu]}</p>
@@ -36,7 +57,31 @@ function ListMebel() {
           )}
         </ul>
       </div>
-      <MebelLink counter={itemMenu} />
+
+      {/* Подменю видов мебели */}
+
+      <div className="popup__list flex">
+        <div onClick={() => setOpenObj(!openObj)} className="popup__list--wrap">
+          <p className="flex">{mainArray[itemObjMenu]}</p>
+          <img
+            src={arrow}
+            className={openObj ? "transImg" : ""}
+            alt="Открыть список"
+          />
+        </div>
+
+        <ul className={openObj ? "open" : ""}>
+          {mainArray.map((item, i) =>
+            mainArray[itemObjMenu] === item ? (
+              ""
+            ) : (
+              <li className="flex" key={i} onClick={() => onClickLinkMenu(i)}>
+                {item}
+              </li>
+            )
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
